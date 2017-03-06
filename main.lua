871c3a4b4fe4c620a3bcf65d7906cdd5d6bdb851
Michael Milwa <mike@milwa.de>
2017-03-06 15:31:46 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,10 +1,7 @@
 -- 
 -- Abstract: wrapper Library Plugin Test Project
 -- 
--- Sample code is MIT licensed, see http://www.coronalabs.com/links/code/license
--- Copyright (C) 2015 Corona Labs Inc. All Rights Reserved.
---
-------------------------------------------------------------
+
 
 -- Load plugin library
 local wrapper = require "plugin.wrapper"
