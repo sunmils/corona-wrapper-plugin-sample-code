ea6b11debfd7354755b677afe4630247d47909ed
Michael Milwa <mike@milwa.de>
2017-03-17 18:46:22 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,16 +3,16 @@
 -- Please read instruction at the top of the wrapper.lua file carefully
 --
 
-local wrapper = require("wrapper")
-
 local _W = display.contentWidth
 local _H = display.contentHeight
 
 -- Try different text, heights, widths, fontSizes etc.
 
+-- size of the frame
 local textFrameWidth = 210
 local textFrameHeight = 210
 
+-- size of the paragraph
 local paragraphWidth = 200
 local paragraphHeight = 200
 
@@ -33,22 +33,22 @@ local timeText
 -- create the paragraph
 local myParagraph = wrapper.newParagraph(
 {
-	text = "Wrapper Class Sample-Text\n\nCorona's framework dramatically increase productivity. \nTasks like animating objects in OpenGL or creating user-interface widgets take only one line of code, and changes are instantly viewable in the Corona Simulator.",
-	width = paragraphWidth,
-	height = paragraphHeight,   -- fontSize will be calculated automatically if set 
-	font = native.systemFont,   -- make sure the used font is installed on your system
-	--fontSize = 30,            -- not needed if height is set 	
-	lineSpace = 2,
-	alignment  = "center",      -- "left", "right", "center" or "justify" 
-	
-	-- these Parameters are just relevant if height is set
-	fontSizeMin = 14,           -- for speed tweaking
-	incrementSize = 2,          -- for speed tweaking
-        fontSizeMax = 30
+    text = "Wrapper Class Sample-Text\n\nCorona's framework dramatically increase productivity. \nTasks like animating objects in OpenGL or creating user-interface widgets take only one line of code, and changes are instantly viewable in the Corona Simulator.",
+    width = paragraphWidth,
+    height = paragraphHeight,   -- fontSize will be calculated automatically if set 
+    font = native.systemFont,   -- make sure the used font is installed on your system
+    --fontSize = 30,            -- not needed if height is set 	
+    lineSpace = 2,
+    alignment  = "justify",      -- "left", "right", "center" or "justify" 
+    
+    -- these Parameters are just relevant if height is set
+    fontSizeMin = 4,           -- for speed tweaking
+    incrementSize = 2,          -- for speed tweaking
+    fontSizeMax = 24
 })
 
 -- display the time needed for wrapping
-timeText = display.newText(tostring(system.getTimer() - t) .. " ms", 0, display.contentHeight - 22, nil, 20)
+timeText = display.newText(tostring(system.getTimer() - t) .. " ms", 0, display.contentHeight-display.screenOriginY - 22, nil, 20)
 timeText.anchorX = 0
 timeText.x = 10
 
@@ -63,4 +63,3 @@ myParagraph.y = _H/3
 myParagraph:setTextColor({0,1,0})
 
 myParagraph:toFront()
-
